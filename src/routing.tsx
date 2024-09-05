dffe6ebde8d973c11d427ad30198f124bb230518
Ogospodigosha <odintsovis@hotmail.com>
2024-09-05 20:41:48 +0300

    add react-hook-form

--- src/routing.tsx
+++ src/routing.tsx
@@ -2,25 +2,40 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import {Button} from "app/ui/button/button";
 import React from "react";
 import {Page} from "app/ui/page/page";
+import {Layout} from "app/ui/layout/layout";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element:
-        <Page mt={'33px'}>
-            <Button variant={"primary"} as={"a"} href={"/blabla"}>
-                hello
-            </Button>
-        </Page>,
+            <Layout>
+                <Page mt={'33px'}>
+                    <Button variant={"primary"} as={"a"} href={"/blabla"}>
+                        hello
+                    </Button>
+                </Page>,
+            </Layout>
     },
     {
-        path: '/login',
+        path: '/home',
         element:
+            <Layout>
+                <Page mt={'33px'}>
+                <div>1231231</div>
+                </Page>
+            </Layout>
+    },
+    {
+        path: '/exercises',
+        element:
+        <Layout>
             <Page mt={'33px'}>
                 <Button variant={"primary"} >
                     hello1
                 </Button>
-            </Page>,
+            </Page>
+        </Layout>
+
     },
 ])
 
